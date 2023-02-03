1d436db8a8b3019e8bc20bc9b955205e9b2f32f0
Warizi <jjin9050@naver.com>
2023-02-03 11:21:15 +0900

    최종

--- script/script.js
+++ script/script.js
@@ -2,12 +2,15 @@
     // 리스트 버튼 좌우 스크롤
     const vectorR = document.querySelector('.vector-r');
     const vectorL = document.querySelector('.vector-l');
-    const goodHeart = document.querySelector('.good');
+    const goodHeart = document.querySelectorAll('.good');
     let count = 0;
 
-    goodHeart.addEventListener('click', function(e) {
-        e.stopPropagation();
-    })
+    for (let a of goodHeart){
+        a.addEventListener('click', function(e) {
+            e.stopPropagation();
+        })
+    }
+    
 
     vectorL.onclick = function() {
         if (count < 0){
@@ -24,18 +27,67 @@
     
 
     //하트창
-    const good = document.querySelector('.good');
-    let goodBtn = 0;
-    good.onclick = function() {
-        if (goodBtn == 0) {
-            good.src = `image/하트a.png`;
-            goodBtn = 1;
+    const good = document.querySelectorAll('.good');
+    let goodBtnA = 0;
+    good[0].onclick = function() {
+        if (goodBtnA == 0) {
+            good[0].src = `image/hearta.png`;
+            goodBtnA = 1;
         } else {
-            good.src = `image/하트b.png`;
-            goodBtn = 0;
+            good[0].src = `image/heartb.png`;
+            goodBtnA = 0;
         }
     }
-
+    let goodBtnB = 0;
+    good[1].onclick = function() {
+        if (goodBtnB == 0) {
+            good[1].src = `image/hearta.png`;
+            goodBtnB = 1;
+        } else {
+            good[1].src = `image/heartb.png`;
+            goodBtnB = 0;
+        }
+    }
+    let goodBtnC = 0;
+    good[2].onclick = function() {
+        if (goodBtnC == 0) {
+            good[2].src = `image/hearta.png`;
+            goodBtnC = 1;
+        } else {
+            good[2].src = `image/heartb.png`;
+            goodBtnC = 0;
+        }
+    }
+    let goodBtnD = 0;
+    good[3].onclick = function() {
+        if (goodBtnD == 0) {
+            good[3].src = `image/hearta.png`;
+            goodBtnD = 1;
+        } else {
+            good[3].src = `image/heartb.png`;
+            goodBtnD = 0;
+        }
+    }
+    let goodBtnE = 0;
+    good[4].onclick = function() {
+        if (goodBtnE == 0) {
+            good[4].src = `image/hearta.png`;
+            goodBtnE = 1;
+        } else {
+            good[4].src = `image/heartb.png`;
+            goodBtnE = 0;
+        }
+    }
+    let goodBtnF = 0;
+    good[5].onclick = function() {
+        if (goodBtnF == 0) {
+            good[5].src = `image/hearta.png`;
+            goodBtnF = 1;
+        } else {
+            good[5].src = `image/heartb.png`;
+            goodBtnF = 0;
+        }
+    }
     //포트폴리오 새창
     const folioOnclick = document.querySelectorAll('.folio-container');
     const onclickNew = document.querySelector('.onclick-new');
@@ -45,7 +97,6 @@
     for (let a of folioOnclick) {
         a.onclick = ()=> {
             const qr = document.querySelector('.new-main-img');
-            // bodyPush.innerHTML = newTab;
             folioClose.style.display = 'block';
             onclickNew.style.display = 'block';
             onclickNew.classList.toggle('.click-hg');
